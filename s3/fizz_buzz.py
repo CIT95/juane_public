472dea9c5d5bd537d80a31eb7d36734ea7ad3a4f
Juan Espinoza <jcarlosspnz963@gmail.com>
2021-01-28 11:49:06 -0800

    Im not sure if I commited but This one was cool

--- s3/fizz_buzz.py
+++ s3/fizz_buzz.py
@@ -1,20 +1,20 @@
-def fizz_buzz(num):
-    if num % 3 == 0 and num % 5 == 0:
+def fizz_buzz(num): #new function
+    if num % 3 == 0 and num % 5 == 0: # if divisible by both 3 and 5 it will print
         print('FIZZBUZZ')
     elif num % 5 == 0:
-        print('BUZZ')
+        print('BUZZ')      # ELIF just in case the first case doesnt work
     elif num % 3 == 0:
         print('FIZZ')
     else:
-        print('nothing:( just your silly number: ' + str(num))
+        print('nothing:( just your silly number: ' + str(num))   # if all fails then it will give me my number back
 
 
 
     
 
 
-def getInfo():
-    num = int(input("Type in a number please: "))
-    fizz_buzz(num)
+def getInfo(): # this was called
+    num = int(input("Type in a number please: ")) #basically asking for number and assigning it as num
+    fizz_buzz(num) # calling another function while adding num
 
-getInfo()
+getInfo() #this is the only thing the code sees and is calling a function called getInfo
