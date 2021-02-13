0f33903afd90f8a9f716a7f637724455b023aaf2
Juan Espinoza <jcarlosspnz963@gmail.com>
2021-02-12 20:39:59 -0800

    This is done

---
+++ w5/list_exericises.py
@@ -0,0 +1,123 @@
+#Exercise 6
+
+lst=[11, 100, 99, 1000, 999]
+answer_1=1st[0]
+
+print(answer_1)
+
+#i didnt use any hints or google. 
+
+lst=[11, 100, 101, 999, 1001]
+
+print(1 st[1])
+
+# i did use the first hint. I guess i just had to remember
+
+lst=[11, 100, 101, 999, 1001]
+#Type your answer here.
+
+answer_1=lst[-1]
+
+print(answer_1)
+
+#This one was without hints or google.
+
+gift_list=['socks', '4K drone', 'wine', 'jam']
+# Type your code here.
+gift_list.append('pajamas')
+
+print(gift_list)
+
+# I used both hints on this one.
+
+gift_list=['socks', '4K drone', 'wine', 'jam']
+# Type your code here.
+gift_list.append(["socks", "tshirt", "pajamas"])
+
+
+print(gift_list)
+
+# This was without any hint or google! leanred from the last one
+
+gift_list=['socks', '4K drone', 'wine', 'jam']
+# Type your code here.
+gift_list.insert(2,"slippers")
+
+
+print(gift_list)
+
+#just used the 1st hint
+
+
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+#  Type your code here.
+
+answer_1=lst.index(8679)
+
+print(answer_1)
+print("done")
+
+#no hints or google on this one
+
+lst=["CRV", "Outback", "XC90", "GL", "Cherokee", "Escalade"]
+#  Type your code here.
+lst.append(["Navigator", "Suburban"])
+
+
+print(lst)
+
+# I happend to use the first hint
+
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+#  Type your code here.
+lst.remove(99)
+
+
+print(lst)
+
+#no hints nor google! Very happy bout that
+
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+#  Type your code here.
+lst.revers()
+
+
+print(lst)
+
+#no google or hints again!
+
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+#  Type your code inside print() function.
+
+answer_1=lst.count(6)
+
+print(answer_1)
+
+#okay i did have to use the solution. For some reason I was very confused.
+
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+#  Type your code on line 4:
+answer_1=sum(lst)
+
+print(answer_1)
+
+#used the 1st hint and got it right away.
+
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+#  Type your code on line 4:
+answer_1=min(lst)
+
+print(answer_1)
+
+#used the first hint this time.
+
+lst=[55, 6, 777, 54, 6, 76, 101, 1, 6, 2, 6]
+
+#  Type your code on line 4:
+answer_1=max(lst)
+
+print(answer_1)
+
+#learned from the previous one and got it without hints or google!
